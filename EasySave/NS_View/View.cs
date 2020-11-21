d5e2da726fb51ea47f395942dcec8badee1690f3
Kaldei <k4ld3i@gmail.com>
2020-11-21 19:02:25 +0100

    Add Initial Architecture
    Initial Architecture Commit

---
+++ EasySave/NS_View/View.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using EasySave.NS_ViewModel;
+
+namespace EasySave.NS_View
+{
+    class View
+    {
+        // --- Attributes ---
+        private ViewModel viewModel;
+
+
+        // --- Constructor ---
+        public View(ViewModel _viewModel)
+        {
+            this.viewModel = _viewModel;
+        }
+
+
+        // --- Methods ---
+    }
+}
---
+++ EasySave/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using EasySave.NS_ViewModel;
+
+namespace EasySave
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Init Program
+            ViewModel viewModel = new ViewModel();
+
+            // Run Program
+            viewModel.Run();
+        }
+    }
+}
