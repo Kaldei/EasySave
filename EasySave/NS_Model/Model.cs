9cdc8e813157573babbd1468d29fe4f1222f3b06
Arthur <caldeireiro.arthur@gmail.com>
2020-11-21 19:02:25 +0100

    Add Initial Architecture
    Initial Architecture Commit

---
+++ EasySave/NS_Model/Model.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasySave.NS_Model
+{
+    class Model
+    {
+        // --- Attributes ---
+
+
+        // --- Constructor ---
+        public Model()
+        {
+        }
+
+
+        // --- Methods ---
+    }
+}
