44c8dc087037476b9f0afb364e78f743558e6d47
Caldei <k4ld3i@gmail.com>
2020-11-21 19:02:25 +0100

    Add Initial Architecture
    Initial Architecture Commit

---
+++ EasySave/NS_ViewModel/ViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using EasySave.NS_Model;
+using EasySave.NS_View;
+
+namespace EasySave.NS_ViewModel
+{
+    class ViewModel
+    {
+        // --- Attributes ---
+        private Model model;
+        private View view;
+
+
+        // --- Constructor ---
+        public ViewModel()
+        {
+            // Instantiate Model & View
+            this.model = new Model();
+            this.view = new View(this);
+        }
+
+
+        // --- Methods ---
+        public void Run()
+        {
+            Console.WriteLine("EasySave is ready to be developed!");
+        }
+    }
+}
