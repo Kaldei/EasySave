a2a28737aaf2bec7811f1a4deeb6237296465f22
Arthur <caldeireiro.arthur@gmail.com>
2020-11-22 19:42:34 +0100

    Add Model Methods
    Add Model Methods (AddWork, RemoveWork, LoadWorks, SaveWorks)
    Correct Backup Type Enumeration

--- EasySave/NS_Model/Work.cs
+++ EasySave/NS_Model/Work.cs
@@ -6,14 +6,21 @@ namespace EasySave.NS_Model
 {
     class Work
     {
-        // Attributes
+        // --- Attributes ---
         public string name { get; set; }
         public string src { get; set; }
         public string dst { get; set; }
         public BackupType backupType { get; set; }
 
 
-        // Constructor
+        // --- Constructors ---
+        // Constructor used by LoadWorks()
+        public Work()
+        {
+
+        }
+
+        // Constructor used by AddWork()
         public Work (string _name, string _src, string _dst, BackupType _backupType)
         {
             this.name = _name;
@@ -23,7 +30,7 @@ namespace EasySave.NS_Model
         }
 
 
-        // Methods 
+        // --- Methods ---
         // DoBackup() ?
     }
 }
