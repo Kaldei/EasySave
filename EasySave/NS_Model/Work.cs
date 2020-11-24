408ba931735312d5ea8111f87b43f777a051c917
Clement Gaston <clem31130@hotmail.fr>
2020-11-24 11:01:31 +0100

    Add display current backup info + Rewrite error code

--- EasySave/NS_Model/Work.cs
+++ EasySave/NS_Model/Work.cs
@@ -8,6 +8,7 @@ namespace EasySave.NS_Model
     class Work
     {
         // --- Attributes ---
+        public int id { get; set; }
         public string name { get; set; }
         public string src { get; set; }
         public string dst { get; set; }
@@ -24,8 +25,9 @@ namespace EasySave.NS_Model
         }
 
         // Constructor used by AddWork()
-        public Work (string _name, string _src, string _dst, BackupType _backupType)
+        public Work (int _id, string _name, string _src, string _dst, BackupType _backupType)
         {
+            this.id = _id;
             this.name = _name;
             this.src = _src;
             this.dst = _dst;
