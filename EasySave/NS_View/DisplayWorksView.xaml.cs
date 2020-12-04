6a1fe7a9fee6e6a9e24f4ef47f54b31546f55a3e
laetitia <laetitiakessas2@gmail.com>
2020-12-04 21:13:59 +0100

    Add Remove Work

--- EasySave/NS_View/DisplayWorksView.xaml.cs
+++ EasySave/NS_View/DisplayWorksView.xaml.cs
@@ -27,14 +27,38 @@ namespace EasySave.NS_View
             DataContext = viewModel;
         }
 
-        private void RemoveOne_Clicked(object sender, RoutedEventArgs e)
-        {
-            //ListWorks.Items.RemoveAt(ListWorks.SelectedIndex);
+        List<int> idWorkSelected = new List<int>();
 
+        private void Remove_Clicked(object sender, RoutedEventArgs e)
+        {
+
+            int Length = ListWorks.SelectedItems.Count;
+            int[] SelectedWorks = new int[Length];
+
+            for (int i = 0; i < Length; i++)
+            {
+                SelectedWorks[i] = ListWorks.Items.IndexOf(ListWorks.SelectedItems[i]);
+            } 
+            viewModel.testRemove(SelectedWorks);
            // ListWorks.SelectedItems;
         }
-        private void RemoveAll_Clicked(object sender, RoutedEventArgs e)
+        private void Save_Clicked(object sender, RoutedEventArgs e)
         {
+
         }
+
+        private void SelectAll_Clicked(object sender, RoutedEventArgs e)
+        {
+
+            if (ListWorks.SelectedItems.Count != viewModel.works.Count)
+            {
+                 ListWorks.SelectAll();            
+            }
+            else
+            {
+                 ListWorks.UnselectAll();
+            }
+        }
+
     }
 }
