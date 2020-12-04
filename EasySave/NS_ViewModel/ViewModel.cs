e6d142ebccfc0f25f2059904161768e2198c23e5
Caldei <k4ld3i@gmail.com>
2020-12-05 00:14:07 +0100

    Add isCrypted attribute form Work
    Adapt Work
    Adapt ViewModel
    Adapt DisplayWorkView
    Adapt AddWorkView

--- EasySave/NS_ViewModel/ViewModel.cs
+++ EasySave/NS_ViewModel/ViewModel.cs
@@ -78,12 +78,12 @@ namespace EasySave.NS_ViewModel
        
         // --- Methods ---
         // Add Work
-        public int AddWork(string _name, string _src, string _dst, BackupType _backupType)
+        public int AddWork(string _name, string _src, string _dst, BackupType _backupType, bool _isCrypted)
         {
             try
             {
                 // Add Work in the program (at the end of the List)
-                this.works.Add(new Work(_name, _src, _dst, _backupType));
+                this.works.Add(new Work(_name, _src, _dst, _backupType, _isCrypted));
                 SaveWorks();
 
                 // Return Success Code
