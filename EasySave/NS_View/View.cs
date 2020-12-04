02c97a829b7049a6643118bf05ab7eb8b8bfc727
Clement Gaston <clem31130@hotmail.fr>
2020-12-04 15:38:25 +0100

    Add First Language Version

--- EasySave/NS_View/View.cs
+++ EasySave/NS_View/View.cs
@@ -1,19 +1,22 @@
 ﻿using System;
 using System.IO;
 using EasySave.NS_ViewModel;
+using System.Text.Json;
 
 namespace EasySave.NS_View
 {
     class View
     {
+        public string language { get; set; }
         // --- Attributes ---
         private ViewModel viewModel;
 
 
         // --- Constructor ---
-        public View(ViewModel _viewModel)
+        public View(ViewModel _viewModel, string _language)
         {
             this.viewModel = _viewModel;
+            this.language = _language;
         }
 
 
@@ -22,25 +25,18 @@ namespace EasySave.NS_View
         public int Menu()
         {
             Console.Clear();
-            Console.WriteLine(
-                "Menu:" +
-                "\n1 - Show all works" +
-                "\n2 - Add a work" +
-                "\n3 - Make a backup" +
-                "\n4 - Remove a work" +
-                "\n5 - Quit");
-
-            return CheckChoiceMenu(Console.ReadLine(), 1, 5);
+            ConsoleUpdate(300);
+            return CheckChoiceMenu(Console.ReadLine(), 1, 6);
         }
 
         //Add work name
         public string AddWorkName()
         {
             Console.Clear();
-            Console.WriteLine("Parameter to add a work:");
+            ConsoleUpdate(301);
             ConsoleUpdate(2);
 
-            Console.WriteLine("\nEnter a name (1 to 20 characters):");
+            ConsoleUpdate(302);
             string name = Console.ReadLine();
 
             //Check if the name is valid
@@ -64,7 +60,7 @@ namespace EasySave.NS_View
         //Add work source
         public string AddWorkSrc()
         {
-            Console.WriteLine("\nEnter directory source. ");
+            ConsoleUpdate(303);
             string src = RectifyPath(Console.ReadLine());
 
             //Check if the path is valid
@@ -79,7 +75,7 @@ namespace EasySave.NS_View
         //Add work destination
         public string AddWorkDst(string _src)
         {
-            Console.WriteLine("\nEnter directory destination.");
+            ConsoleUpdate(304);
             string dst = RectifyPath(Console.ReadLine());
 
             //Check if the path is valid
@@ -125,10 +121,7 @@ namespace EasySave.NS_View
         //Add work backup type
         public int AddWorkBackupType()
         {
-            Console.WriteLine(
-                "\nChoose a type of Backup: " +
-                "\n1.Full " +
-                "\n2.Differential");
+            ConsoleUpdate(305);
             return CheckChoiceMenu(Console.ReadLine(), 0, 2);
         }
 
@@ -139,7 +132,7 @@ namespace EasySave.NS_View
 
             if (length >= 1 && length <= 20)
             {
-                if (!this.viewModel.model.works.Exists(work => work.name == _name))
+                if (!this.viewModel.works.Exists(work => work.name == _name))
                 {
                     return true;
                 }
@@ -167,7 +160,7 @@ namespace EasySave.NS_View
         //Display all works 
         private void LoadWorks(int _shift)
         {
-            var works = this.viewModel.model.works;
+            var works = this.viewModel.works;
 
             for (int i = 0; i < works.Count; i++)
             {
@@ -182,7 +175,7 @@ namespace EasySave.NS_View
         public void DisplayWorks()
         {
             Console.Clear();
-            Console.WriteLine("Work list :");
+            ConsoleUpdate(307);
 
             //Display all works 
             LoadWorks(1);
@@ -193,30 +186,41 @@ namespace EasySave.NS_View
         public int LaunchBackupChoice()
         {
             Console.Clear();
-            Console.WriteLine(
-                "Choose the work to save : " +
-                "\n\n1 - all");
+            ConsoleUpdate(306);
 
             //Display all works 
             LoadWorks(2);
             ConsoleUpdate(2);
 
             //Check if the user's input is a valid integer
-            return CheckChoiceMenu(Console.ReadLine(), 0, this.viewModel.model.works.Count + 1);
+            return CheckChoiceMenu(Console.ReadLine(), 0, this.viewModel.works.Count + 1);
         }
 
         //Choose the work to remove
         public int RemoveWorkChoice()
         {
             Console.Clear();
-            Console.WriteLine("Choose the work to remove :");
+            ConsoleUpdate(308);
 
             //Display all works 
             LoadWorks(1);
             ConsoleUpdate(2);
 
             //Check if the user's input is a valid integer
-            return CheckChoiceMenu(Console.ReadLine(), 0, this.viewModel.model.works.Count);
+            return CheckChoiceMenu(Console.ReadLine(), 0, this.viewModel.works.Count);
+        }
+
+        //Choose the language to display
+        public int LanguageChoice()
+        {
+            Console.Clear();
+            ConsoleUpdate(309);
+            ConsoleUpdate(2);
+            Console.WriteLine("1. English");
+            Console.WriteLine("2. Français");
+
+            //Check if the user's input is a valid integer
+            return CheckChoiceMenu(Console.ReadLine(), 0, 2);
         }
 
         //Check if the input is a integer and in the good range
@@ -301,9 +305,35 @@ namespace EasySave.NS_View
         }
 
         //Display message on the console
-        public void ConsoleUpdate(int _id) 
+        public void ConsoleUpdate(int _id)
         {
-            if (_id < 100)
+            if (_id < 100 && language == "FR")
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                switch (_id)
+                {
+                    //Information message
+                    case 1:
+                        Console.WriteLine("\nAppuyer sur Entrée pour afficher le menu . . .");
+                        Console.ReadLine();
+                        break;
+
+                    case 2:
+                        Console.WriteLine("\n(Appuyer sur 0 pour revenir au menu)");
+                        break;
+
+                    case 3:
+                        Console.Clear();
+                        Console.WriteLine("\nInformation de la sauvegarde :");
+                        break;
+
+                    case 4:
+                        Console.WriteLine("\nAppuyer sur Entrée pour en voir plus . . .");
+                        Console.ReadLine();
+                        break;
+                }
+            }
+            else if (_id < 100)
             {
                 Console.ForegroundColor = ConsoleColor.Yellow;
                 switch (_id)
@@ -326,6 +356,40 @@ namespace EasySave.NS_View
                     case 4:
                         Console.WriteLine("\nPress Enter key to show more . . .");
                         Console.ReadLine();
+                        break;
+                }
+            }
+            else if (_id < 200 && language == "FR")
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                switch (_id)
+                {
+                    // Success message from 100 to 199
+                    case 100:
+                        Console.WriteLine("\n----- BIENVENUE SUR EASYSAVE -----");
+                        ConsoleUpdate(1);
+                        break;
+
+                    case 101:
+                        Console.WriteLine("\nLe travail a été correctement ajouter !");
+                        ConsoleUpdate(1);
+                        break;
+
+                    case 102:
+                        Console.WriteLine("\nLe travail a été correctement sauvegarder !");
+                        break;
+
+                    case 103:
+                        Console.WriteLine("\nLe travail a été correctement supprimer !");
+                        ConsoleUpdate(1);
+                        break;
+
+                    case 104:
+                        Console.WriteLine("\nSauvegarde réussite !");
+                        break;
+
+                    case 105:
+                        Console.WriteLine("\nAucune modification n'a été faite depuis la dernière sauvegarde complète !\n");
                         break;
                 }
             }
@@ -355,7 +419,7 @@ namespace EasySave.NS_View
                         break;
 
                     case 104:
-                        Console.WriteLine("\nBackup success !");
+                        Console.WriteLine("\nBackup success!");
                         break;
 
                     case 105:
@@ -363,7 +427,98 @@ namespace EasySave.NS_View
                         break;
                 }
             }
-            else
+            else if(_id < 300 && language == "FR")
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                switch (_id)
+                {
+                    // Error message from 200 to 299
+                    case 200:
+                        Console.WriteLine("\nS'il vous plaît, restaurer votre fichier JSON de sauvegarde.");
+                        ConsoleUpdate(1);
+                        break;
+
+                    case 201:
+                        Console.WriteLine("\nEchec de l'ajout d'un travail.");
+                        ConsoleUpdate(1);
+                        break;
+
+                    case 202:
+                        Console.WriteLine("\nEchec de la sauvegarde du travail.");
+                        ConsoleUpdate(1);
+                        break;
+
+                    case 203:
+                        Console.WriteLine("\nEchec de la suppression d'un travail.");
+                        ConsoleUpdate(1);
+                        break;
+
+                    case 204:
+                        Console.WriteLine("\nLa liste de travail est vide.");
+                        ConsoleUpdate(1);
+                        break;
+
+                    case 205:
+                        Console.WriteLine("\nLa liste de travail est pleine.");
+                        ConsoleUpdate(1);
+                        break;
+
+                    case 206:
+                        Console.WriteLine("\nS'il vous plaît, entrer une option valide :");
+                        break;
+
+                    case 207:
+                        Console.WriteLine("\nEchec de transférer un fichier, le chemin de destination ou de source n'existe pas.");
+                        break;
+
+                    case 208:
+                        Console.WriteLine("\nLe type de sauvegarde selection n'existe pas.");
+                        break;
+
+                    case 209:
+                        Console.WriteLine("\nLa copie d'un fichier a échoué.");
+                        ConsoleUpdate(1);
+                        break;
+
+                    case 210:
+                        Console.WriteLine("\nEchec de la création d'un dossier de sauvegarde.");
+                        ConsoleUpdate(1);
+                        break;
+                    case 211:
+                        Console.WriteLine("\nLe dossier n'existe pas. S'il vous plaît, entrer un chemin source valide.");
+                        break;
+
+                    case 212:
+                        Console.WriteLine("\nVeuillez choisir un chemin de destination différent de celui du source :");
+                        break;
+
+                    case 213:
+                        Console.WriteLine("\nLe dossier n'existe pas. S'il vous plaît, entrer un chemin de destination valide.");
+                        break;
+
+                    case 214:
+                        Console.WriteLine("\nLe nom du travail est déjà utiliser. Veuillez en choisir un autre :");
+                        break;
+
+                    case 215:
+                        Console.WriteLine("\nEntrer un nom valide (1 à 20 caractères) :");
+                        break;
+
+                    case 216:
+                        Console.WriteLine("\nLa sauvegarde est terminé avec des erreurs.");
+                        break;
+
+                    case 217:
+                        Console.WriteLine("\nLe dossier de destination ne peut être à l'intérieur du dossier source.");
+                        break;
+
+                    default:
+                        Console.WriteLine("\nEchec : Erreur inconnue.");
+                        ConsoleUpdate(1);
+                        break;
+                }
+            }
+            else if (_id < 300)
             {
                 Console.ForegroundColor = ConsoleColor.Red;
                 switch (_id)
@@ -437,7 +592,7 @@ namespace EasySave.NS_View
                         break;
 
                     case 215:
-                        Console.WriteLine("\nEnter a VALID name (1 to 20 characters):");
+                        Console.WriteLine("\nEnter a valid name (1 to 20 characters):");
                         break;
 
                     case 216:
@@ -451,6 +606,120 @@ namespace EasySave.NS_View
                     default:
                         Console.WriteLine("\nFailed : Error Unknown.");
                         ConsoleUpdate(1);
+                        break;
+                }
+            }
+            else if (language == "FR")
+            {
+                switch (_id)
+                {
+                    case 300:
+                        Console.WriteLine(
+                            "Menu:" +
+                            "\n1 - Voir les travaux" +
+                            "\n2 - Ajouter un travail" +
+                            "\n3 - Lancer une sauvegarde" +
+                            "\n4 - Supprimer un travail" +
+                            "\n5 - Choisir une langue" +
+                            "\n6 - Quitter");
+                        break;
+
+                    case 301:
+                        Console.WriteLine("Paramètres pour ajouter un travail :");
+                        break;
+
+                    case 302:
+                        Console.WriteLine("\nEntrer un nom (1 à 20 caractères) :");
+                        break;
+
+                    case 303:
+                        Console.WriteLine("\nEntrer le chemin du dossier source :");
+                        break;
+
+                    case 304:
+                        Console.WriteLine("\nEntrer le chemin du dossier cible :");
+                        break;
+
+                    case 305:
+                        Console.WriteLine(
+                            "\nChoisir un type de sauvegarde: " +
+                            "\n1.Complète " +
+                            "\n2.Différentiel");
+                        break;
+
+                    case 306:
+                        Console.WriteLine(
+                            "Choisir un travail à sauvegarder : " +
+                            "\n\n1 - tous");
+                        break;
+
+                    case 307:
+                        Console.WriteLine("Liste de travail :");
+                        break;
+
+                    case 308:
+                        Console.WriteLine("Choisir un travail à supprimer :");
+                        break;
+
+                    case 309:
+                        Console.WriteLine("Choisir une langue :");
+                        break;
+                }
+            }
+            else //if(language == "EN")
+            {
+                switch (_id)
+                {
+                    case 300:
+                        Console.WriteLine(
+                            "Menu:" +
+                            "\n1 - Show all works" +
+                            "\n2 - Add a work" +
+                            "\n3 - Make a backup" +
+                            "\n4 - Remove a work" +
+                            "\n5 - Choose language" +
+                            "\n6 - Quit");
+                        break;
+
+                    case 301:
+                        Console.WriteLine("Parameter to add a work:");
+                        break;
+
+                    case 302:
+                        Console.WriteLine("\nEnter a name (1 to 20 characters):");
+                        break;
+
+                    case 303:
+                        Console.WriteLine("\nEnter directory source:");
+                        break;
+
+                    case 304:
+                        Console.WriteLine("\nEnter directory destination:");
+                        break;
+
+                    case 305:
+                        Console.WriteLine(
+                            "\nChoose a type of Backup: " +
+                            "\n1.Full " +
+                            "\n2.Differential");
+                        break;
+
+                    case 306:
+                        Console.WriteLine(
+                            "Choose the work to save : " +
+                            "\n\n1 - all");
+                        break;
+
+                    case 307:
+                        Console.WriteLine("Work list :");
+                        break;
+
+                    case 308:
+                        Console.WriteLine("Choose the work to remove :");
+                        break;
+
+                    case 309:
+                        Console.WriteLine("Choose a language:");
                         break;
                 }
             }
