1575153a0fb6ab647f16a04e22dec9448a5e5bd5
laetitia <laetitiakessas2@gmail.com>
2020-12-05 15:33:02 +0100

    Add Language
    Add Language ( fr-FR and en-US )
    Update AddWorkView
    Update DisplayWorksView

--- EasySave/MainWindow.xaml.cs
+++ EasySave/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,19 +23,12 @@ namespace EasySave
     /// </summary>
     public partial class MainWindow : Window
     {
+
         public MainWindow()
         {
             InitializeComponent();
-
+            Langs.Lang.Culture = new CultureInfo("en-US");
             DataContext = new DisplayWorksView();
-
-            /*            // Init Program
-                        ViewModel viewModel = new ViewModel();
-                        DataContext = new ShowWorksView();
-
-
-                        // Run Program
-                        viewModel.Run();*/
         }
 
         private void AddWorkButton(object sender, RoutedEventArgs e)
@@ -45,6 +39,17 @@ namespace EasySave
         private void DisplayWorksButton(object sender, RoutedEventArgs e)
         {
             DataContext = new DisplayWorksView();
+        }
+
+        private void SelectFrLanguage(object sender, RoutedEventArgs e)
+        {
+            Langs.Lang.Culture = new CultureInfo("fr-FR");
+
+        }
+
+        private void SelectEnLanguage(object sender, RoutedEventArgs e)
+        {
+            Langs.Lang.Culture = new CultureInfo("en-US");
         }
 
     }
