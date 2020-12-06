d4de4975b39bc6a2c7995858f2871e20ea1a04a0
laetitia <laetitiakessas2@gmail.com>
2020-12-06 20:32:00 +0100

    Add fr and us messages

--- EasySave/NS_View/ErrorView.xaml.cs
+++ EasySave/NS_View/ErrorView.xaml.cs
@@ -11,6 +11,10 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using EasySave.NS_Model;
+using EasySave.Langs;
+using System.Resources;
+using System.Globalization;
+using System.Collections;
 
 namespace EasySave.NS_View
 {
@@ -27,7 +31,73 @@ namespace EasySave.NS_View
 
         public void DisplayErrorMsg(string _errorName)
         {
-            MessageBox.Show(_errorName);
+
+            switch (_errorName)
+            {
+                case "errorAddWork":
+                    MessageBox.Show(Lang.errorAddWork);
+                    break;
+
+                case "errorRemoveWork":
+                    MessageBox.Show(Lang.errorRemoveWork);
+                    break;
+
+                case "businessSoftwareOn":
+                    MessageBox.Show(Lang.businessSoftwareOn);
+                    break;
+
+                case "noSelectedWork":
+                    MessageBox.Show(Lang.noSelectedWork);
+                    break;
+
+                case "unavailableSrcPath":
+                    MessageBox.Show(Lang.unavailableSrcPath);
+                    break;
+
+                case "unavailableDstPath":
+                    MessageBox.Show(Lang.unavailableDstPath);
+                    break;
+
+                case "unavailableBackupType":
+                    MessageBox.Show(Lang.unavailableBackupType);
+                    break;
+
+                case "noChangeSinceLastBackup":
+                    MessageBox.Show(Lang.noChangeSinceLastBackup);
+                    break;
+
+                case "cannotCreateDstFolder":
+                    MessageBox.Show(Lang.cannotCreateDstFolder);                  
+                    break;
+
+                case "backupFinishedWithError":
+                    MessageBox.Show(Lang.backupFinishedWithError);
+                    break;
+
+                case "noSpaceDstFolder":
+                    MessageBox.Show(Lang.noSpaceDstFolder);
+                    break;
+
+                case "diskError":
+                    MessageBox.Show(Lang.diskError);
+                    break;
+
+                case "cryptoSoftPathNotFound":
+                    MessageBox.Show(Lang.cryptoSoftPathNotFound);
+                    break;
+
+                case "cryptoSoftPathError":
+                    MessageBox.Show(Lang.cryptoSoftPathError);
+                    break;
+
+                default:
+                    MessageBox.Show(Lang.unknownError);
+                    break;
+            }
+            
         }
+
+
+
     }
 }
