ea0f85867235b5b0c279e6c170bf7e9e4fe5ba5f
laetks <44748376+laetks@users.noreply.github.com>
2020-12-12 20:12:25 +0100

    Add WorkState enum

---
+++ EasySave_Code/NS_Model/WorkState.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasySave.NS_Model
+{
+    public enum WorkState
+    {
+        FINISH,
+        RUN,
+        PAUSE,
+        CANCEL
+    }
+}
