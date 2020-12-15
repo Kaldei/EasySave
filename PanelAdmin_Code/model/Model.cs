efa020307c600e84550390c629624494fc530c8e
Anthony Lorendeaux <anthony.lorendeaux@free.fr>
2020-12-15 17:51:24 +0100

    Add Panel Admin to EasySave

---
+++ PanelAdmin_Code/model/Model.cs
@@ -0,0 +1,45 @@
+﻿
+using PanelAdmin.Observable;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
+using System.Text.Json;
+
+namespace PanelAdmin.model
+{ 
+
+    public class Model : ObservableObject
+    {
+        // --- Attributes ---
+        // Prepare options to indent JSON Files
+        private JsonSerializerOptions jsonOptions = new JsonSerializerOptions()
+        {
+            WriteIndented = true
+        };
+
+        private ObservableCollection<Work> Works { get; set;}
+        public ObservableCollection<Work> works {
+            get {
+                return Works; 
+            }
+            set
+            {
+                if(Works != value)
+                {
+                    Works = value;
+                    OnPropertyChanged("works");
+                }
+            }
+        }
+
+
+        // --- Constructor ---
+        public Model()
+        { 
+            // Initialize Work List
+            works = new ObservableCollection<Work>();
+        }
+    }
+}
